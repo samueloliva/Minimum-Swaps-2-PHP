794f1c10aa66e31efd9c3db4a5abb9f60f42c8d6
Samuel Zanferdini Oliva <2183164+samueloliva@users.noreply.github.com>
2020-04-25 10:50:24 -0300

    Code solution
    Code solution of the problem

---
+++ minimum-swaps-2.php
@@ -0,0 +1,43 @@
+<?php
+
+// Complete the minimumSwaps function below.
+function minimumSwaps($arr) {   
+    $ctr = 0;
+    $swapping = true; 
+    while ($swapping) {
+        $swapping = false;
+        $pos = 1;
+        foreach( $arr as $el ) {
+            if ( $pos != $el ) {
+                $curr = array_search($pos, $arr);
+                $temp = $arr[$pos-1];
+                $arr[$pos-1]= $pos;
+                $arr[$curr] = $temp;  
+                $swapping = true;
+                $ctr += 1;
+                break;        
+            }
+            $pos += 1;
+        }
+    }
+    return $ctr;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+fscanf($stdin, "%[^\n]", $arr_temp);
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$res = minimumSwaps($arr);
+
+fwrite($fptr, $res . "\n");
+
+fclose($stdin);
+fclose($fptr);
+
+?>
